d4867adcadf42e51315cdef4f73759bd63c3e3ca
aishagilbert <aikgilbe@ucsc.edu>
2022-10-12 11:12:12 -0700

    no

---
+++ lab4/js/lab.js
@@ -0,0 +1,15 @@
+/**
+ * Short description. (use period)
+ *
+ * @namespace realName
+ * @memberof  parentNamespace
+ *
+ * @since x.x.x
+ *
+ * @property {type} key Description.
+ */
+ // Have a 3 second timer
+    // Allow user to select one of the options during the 3 second count down
+        // Have a randomized option selected by the computer during the 3 second countdown
+            // Show the chosen options on screen at the same time
+            // Winner or loser title will be displayed based on results 
