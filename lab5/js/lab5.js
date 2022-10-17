3e66b81004e7ae421cbcf05a7aa536a24f07710c
aishagilbert <aikgilbe@ucsc.edu>
2022-10-17 10:35:26 -0700

    yes

---
+++ lab5/js/lab5.js
@@ -0,0 +1,12 @@
+alert("Hello World");
+make = "subaru";
+model = "Forester";
+color = "Silver";
+year = "2007";
+age = 2022 - year;
+
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
