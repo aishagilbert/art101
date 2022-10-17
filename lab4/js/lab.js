9c0c265373703c2728edf3290eac0831a2e7ced5
aishagilbert <aikgilbe@ucsc.edu>
2022-10-17 10:19:39 -0700

    a

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -9,7 +9,7 @@
  * @property {type} key Description.
  */
  // Have a 3 second timer
-    // Allow user to select one of the options during the 3 second count down
-        // Have a randomized option selected by the computer during the 3 second countdown
-            // Show the chosen options on screen at the same time
-            // Winner or loser title will be displayed based on results 
+// Allow user to select one of the options during the 3 second count down
+// Have a randomized option selected by the computer during the 3 second countdown
+// Show the chosen options on screen at the same time
+// Winner or loser title will be displayed based on results
