5d2479da861864f34a4f4e09b364a8ce4e1138f3
aishagilbert <aikgilbe@ucsc.edu>
2022-10-21 18:53:19 -0700

    h

---
+++ lab5/js/lab.js
@@ -0,0 +1,21 @@
+/*
+* Author: Aisha Gilbert and Jia Xu
+* Created: 17 October
+* LIcense: Public Domain
+*/
+
+//Define Variables
+make = "Subaru";
+model = "Forester";
+color = "Silver";
+year = 2007;
+
+//calculate
+age = 2022 - year;
+
+//output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
